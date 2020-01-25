e9ee7afb58a99369e1bcf383d593645a272a3799
Mateusz Hamera <mateusz.hamera@wp.pl>
2020-01-25 10:18:19 +0100

    Added Views and ViewModels to App

---
+++ RecipeApp.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RecipeApp.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("RecipeApp.Tests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("90fe9a67-ca45-4239-a6d7-ffcd94c1997e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RecipeApp/View/Add recipe/ProductsInRecipe.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RecipeApp.View.Add_recipe
+{
+    /// <summary>
+    /// Interaction logic for ProductsInRecipe.xaml
+    /// </summary>
+    public partial class ProductsInRecipe : UserControl
+    {
+        public ProductsInRecipe()
+        {
+            InitializeComponent();
+        }
+    }
+}
